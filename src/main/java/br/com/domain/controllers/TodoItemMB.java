da7d7d185aa59716fef9fc27205b93effc075b8f
Douglas08Santos <santosdouglas0808@gmail.com>
2020-03-14 21:11:32 -0300

    TodoItemMB - operações de adicionar, remover e atualizar tarefas

--- src/main/java/br/com/domain/controllers/TodoItemMB.java
+++ src/main/java/br/com/domain/controllers/TodoItemMB.java
@@ -5,11 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import br.com.domain.model.TodoItem;
 import br.com.domain.util.HibernateUtil;
@@ -23,6 +26,10 @@ public class TodoItemMB{
 	private List<TodoItem> tarefas;
 	
 	
+	public TodoItemMB() {
+		tarefa = new TodoItem();
+	}
+
 	public TodoItem getTarefa() {
 		return tarefa;
 	}
@@ -49,10 +56,31 @@ public class TodoItemMB{
 		atualizar();
 		
 	}
+	
+	public void remover() {
+		EntityManager em = HibernateUtil.getEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		tarefa = em.merge(tarefa);
+		em.remove(tarefa);
+		transaction.commit();
+		em.close();
+		tarefa = new TodoItem();
+		atualizar();
+	}
 
-	private void atualizar() {
-		// TODO Auto-generated method stub
-		
+	public void atualizar() {
+		EntityManager em = HibernateUtil.getEntityManager();
+		String sqlQuery = "select * from tarefa";
+		Query query = em.createNativeQuery(sqlQuery, TodoItem.class);
+		this.tarefas = query.getResultList();
+		em.close();
+	}
+	
+	public void mensagem() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.addMessage(null, new FacesMessage("OK", "OK"));
+		System.out.println("mensagem inserida");
 	}
 	
 	
